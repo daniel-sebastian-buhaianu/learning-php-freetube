b43bc1786cc202bd4100faebfb010ed8092c1e04
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-05-08 10:24:15 +0300

    finished responsive layout for index page

--- js/index.js
+++ js/index.js
@@ -11,18 +11,19 @@ import { SignIn } from './signIn.component.js';
 const main = () => {
 
 	const isMember = 0;
-	
+	const nVideos = calcVideosToDisplay();
+
 	const videosDownloaded = document.getElementById('videosDownloaded');
 	const downloadedVideos = document.createElement('div');
 	downloadedVideos.setAttribute('class', 'videos');
 	videosDownloaded.appendChild(downloadedVideos);
-	displayNVideos(5, downloadedVideos, isMember, 1);
+	displayNVideos(nVideos, downloadedVideos, isMember, 1);
 
 	const videosNotDownloaded = document.getElementById('videosNotDownloaded');
 	const notDownloadedVideos = document.createElement('div');
 	notDownloadedVideos.setAttribute('class', 'videos');
 	videosNotDownloaded.appendChild(notDownloadedVideos);
-	displayNVideos(5, notDownloadedVideos, isMember, 0);
+	displayNVideos(nVideos, notDownloadedVideos, isMember, 0);
 
 	SearchBar(isMember, downloadedVideos, notDownloadedVideos);
 
@@ -32,3 +33,14 @@ const main = () => {
 };
 
 loadGapi(main);
+
+const calcVideosToDisplay = () => {
+
+	const width = window.screen.width;
+
+	if (width >= 1500) { return 12; }
+
+	if (width >= 768) { return 6; }
+
+	return 3;
+}
